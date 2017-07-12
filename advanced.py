95658552dbc10e561bb6e0e313d807e8b3cf6a66
jerryxhe <rebound1618@gmail.com>
2017-07-12 12:38:46 -0400

    Create advanced.py
    advanced parsing tools

---
+++ advanced.py
@@ -0,0 +1,43 @@
+import re
+
+class OrPattern(BasePattern):
+	def __init__(self, pat1, pat2):
+		self.pat1=pat1
+		self.pat2=pat2
+	def __eq__(self, other):
+		if self.pat1==other or self.pat2==other:
+			return True
+		return False
+
+class RegExpPattern(BasePattern):
+	"""TODO: class-level caching"""
+	def __init__(self, pat_str):
+		self.re = re.compile(pat_str, re.UNICODE | re.IGNORECASE)
+		self.name="_pattern:"+pat_str
+	def __eq__(self, sentence_context): 
+		if self.re.match(sentence_context):
+			return True
+		return False
+
+class BasePattern:
+		__metaclass__= ABCMeta
+		@staticmethod
+		def instantiate_the_uninstantiated(self, o):
+			if isinstance(o, types.ClassType): # instantiating with all default parameters
+				return o()
+			return o
+		@staticmethod
+		def default_accessor(self, _dict):
+			return _dict['BodyText']
+		@abstractmethod
+		def __eq__(self, other):
+			return False
+		def __or__(self, pattern2):
+			return OrPattern(self, pattern2)
+		def nword(self):
+			if hasattr(self, 'nwords'):
+				return self.nwords
+			return 1
+		def _(self):
+			return 1
+ 
